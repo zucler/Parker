53163fa7e63c8b0ba5f208cce825de27013df5be
Maxim Pak <zucler@gmail.com>
2015-12-13 16:55:09 +1100

    CPRK-7 Added admin interface to manage Parkings

--- parker/models.py
+++ parker/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
+RATE_TYPES = (
+    ('WILSON', 'Wilson'),
+    ('SECURE', 'Secure Parking'),
+)
+
 
 class Parking(models.Model):
     parkingID = models.AutoField(primary_key=True, unique=True)
+    label = models.CharField(max_length=500)
     address = models.TextField()
     geo_location = models.CharField(max_length=200)
-    type = models.CharField(max_length=150)
-    label = models.CharField(max_length=500)
+    type = models.CharField(max_length=150, choices=RATE_TYPES)
     places_of_interest = models.TextField()
     uri = models.TextField()
+
+    def __str__(self):
+        return self.label
 
 
 class RateType(models.Model):
@@ -17,7 +25,7 @@ class RateType(models.Model):
     day_of_week = models.CharField(max_length=50)
     start_time = models.TimeField()
     end_time = models.TimeField()
-    type = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, choices=RATE_TYPES)
 
 
 class RatePrices(models.Model):
