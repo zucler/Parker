79f727bf741a2d305530532dc6c64c6c5e3137a6
Maxim Pak <zucler@gmail.com>
2015-12-28 18:23:43 +1100

    CPRK-2 Saving parking rates data into DB

--- scripts/loadParkingRates.py
+++ scripts/loadParkingRates.py
@@ -6,7 +6,7 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.support.ui import WebDriverWait
 
 import parker.wsgi
-from parker.models import Parking, RateType
+from parker.models import Parking, RateType, RatePrice
 from parker.parser import WillsonsRatesParser
 
 carparkings = Parking.objects.all()
@@ -19,30 +19,24 @@ for carpark in carparkings:
         WebDriverWait(browser, timeout=10).until(
                 lambda x: x.find_element_by_class_name('rates'))
         # store it as string variable
-        page_source = browser.page_source
         parser = WillsonsRatesParser()
-        rates = parser.get_prices_information(page_source)
+        rates = parser.get_prices_information(browser.page_source)
         # save rates into DB
-        for rate_name, rate_type in rates.types.items():
-            carpark_rate_type = RateType(parkingID=carpark, label=rate_name, type=rate_type)
-            # @TODO: Load this data from website
-            # Hardcoded data to allow to finish the task
-            if rate_name == "Early Bird":
-                carpark_rate_type.start_time = "07:00"
-                carpark_rate_type.end_time = "09:00"
-                carpark_rate_type.day_of_week = "Mon"
-            elif rate_name == "Night":
-                carpark_rate_type.start_time = "18:00"
-                carpark_rate_type.end_time = "23:59"
-                carpark_rate_type.day_of_week = "Mon"
-            elif rate_name == "Casual":
-                carpark_rate_type.start_time = "09:00"
-                carpark_rate_type.end_time = "18:00"
-                carpark_rate_type.day_of_week = "Mon"
-            elif rate_name == "Weekend":
-                carpark_rate_type.start_time = "06:00"
-                carpark_rate_type.end_time = "23:59"
-                carpark_rate_type.day_of_week = "Sun"
+        for rate_name, rate_data in rates.types.items():
+            if rate_data['type'] == "Flat":
+                for day_of_week in rate_data['days_range']:
+                    carpark_rate = RateType(parkingID=carpark, label=rate_name, type=rate_data['type'],
+                                            day_of_week=day_of_week, start_time=rate_data['start'],
+                                            end_time=rate_data['end'])
+                    carpark_rate.save()
 
-                # carpark_rate_type.save()
-                # print("Carpark rate id is " + str(carpark_rate_type.rateID))
+                    carpark_rate_price = RatePrice(rateID=carpark_rate, duration=0, price=rates.rates[rate_name][day_of_week])
+                    carpark_rate_price.save()
+            elif rate_data['type'] == "Hourly":
+                carpark_rate = RateType(parkingID=carpark, label=rate_name, type=rate_data['type'], day_of_week="",
+                                        start_time=rate_data['start'], end_time=rate_data['end'])
+                carpark_rate.save()
+
+                for price in rates.rates[rate_name].items():
+                    carpark_rate_price = RatePrice(rateID=carpark_rate, duration=price[0], price=price[1])
+                    carpark_rate_price.save()
