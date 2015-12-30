543f2106a5a3fd34e8e643f7aac5f5f5215acb06
Maxim Pak <mpak@switch.tv>
2015-12-30 17:36:27 +1100

    CPRK-13 Added sample view to display parking details Added debug toolbar to troubleshoot Django templates Configured PyCharm to debug web apps

--- parker/urls.py
+++ parker/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'^test_search/', views.test, name='Test search'),
+    url(r'^details/(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='vote')
+
 ]
