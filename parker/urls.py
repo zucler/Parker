04d0e50bd04cb82c686942470b3a3e9a4acabc5a
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-17 16:53:41 +0700

    Init commit

--- parker/urls.py
+++ parker/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^test_search/', views.test, name='Test search'),
     url(r'^details/(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='vote')
-
+    url(r'^search/', views.search, name='Parking search')
 ]
