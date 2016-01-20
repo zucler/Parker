fb9f103b89197b2dfd2a77b180215d90123b173c
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-20 23:09:43 +0700

    Get the viewable boundaries as lat-long

--- parker/settings.py
+++ parker/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_URL = '/static/'
+STATIC_URL = '/extras/'
 
 TEMPLATES = [
     {
