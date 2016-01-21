fc414fb4e713a371e9010fe126000ae379d18ca9
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-21 22:27:45 +0700

    API now working Need to refactor a lot Current request example: http://127.0.0.1:8000/api/parkings?minlat=-89&maxlat=89&minlong=-179&maxlong=179

--- parker/dbhelpers.py
+++ parker/dbhelpers.py
@@ -1,9 +1,8 @@
-__author__ = 'ctepasiam2'
 from django.db.models import Model
 from parker.models import Parking, RatePrice, RateType
 
 
-def geo_search(self, min_lat, max_lat, min_long, max_long):
+def geo_search(min_lat, max_lat, min_long, max_long):
     """
     We assume that search boundaries are small enough so there is no need to do complex
     spherical calculations of latitude and longitude.
@@ -11,6 +10,6 @@ def geo_search(self, min_lat, max_lat, min_long, max_long):
     Returns: All Parking's model objects within given boundary
     """
 
-    parkings = Parking.object.filter(lat__gte=min_lat, lat__lte=max_lat, long__gte=min_long, long__lte=max_long)
+    parkings = Parking.objects.filter(lat__gte=min_lat, lat__lte=max_lat, long__gte=min_long, long__lte=max_long)
 
     return parkings.all()
