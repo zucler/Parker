3d09b66f91b7a28d1d063f207855c9a917bc9a80
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-21 14:06:58 +0700

    REST framework added

--- parker/settings.py
+++ parker/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'debug_toolbar',
     'polls',
     'parker',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,12 @@ TEMPLATES = [
         },
     },
 ]
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
