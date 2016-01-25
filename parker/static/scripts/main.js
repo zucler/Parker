fc91e8f470314c63e430082db0bdf81f19ed551c
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-25 21:52:32 +0700

    Place search example

--- parker/static/scripts/main.js
+++ parker/static/scripts/main.js
@@ -116,6 +116,8 @@ function createSearchControl(parentDiv) {
 	attachSearchControl(controlText);
 }
 
+var markers = new Array();
+
 function attachSearchControl (parentDiv) {
 	var AustraliaBounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(-40, 111),
@@ -124,6 +126,50 @@ function attachSearchControl (parentDiv) {
 	var searchBox = new google.maps.places.SearchBox(parentDiv, {
 		bounds: AustraliaBounds
 	});
+	
+	  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
 	
 function globalInit() {
