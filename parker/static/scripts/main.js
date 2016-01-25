9aa8eb6893bb7dd09b2805dce7ccf4f1bddf2666
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-25 13:58:19 +0700

    Geocoding input added.

--- parker/static/scripts/main.js
+++ parker/static/scripts/main.js
@@ -9,7 +9,12 @@ var markersArray = new Array();
 function initMap() {
   map = new google.maps.Map( document.getElementById('map') , {
     center: {lat: -33.870384731980205, lng: 151.200569099426275},
-    zoom: 14
+    zoom: 14,
+		mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DEFAULT,
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+    },
   });
 	
 	function onBoundsChanged(){
@@ -39,7 +44,12 @@ function initMap() {
 		clearTimeout(boundsEventTimer); 
     boundsEventTimer = setTimeout(onBoundsChanged, boundsTimerDelay)
   });
-
+	
+	// Create search box
+	var controlDiv = document.createElement('div');
+	createSearchControl(controlDiv);
+	controlDiv.index = 1;
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(controlDiv);
 }
 
 function findParkingsByLatlong(minlat, maxlat, minlong, maxlong) {
@@ -52,7 +62,6 @@ function findParkingsByLatlong(minlat, maxlat, minlong, maxlong) {
 						"maxlong": maxlong },
 		success: function(data) {
 			if (data.length > 0) {
-				//alert(data[0]['lat'] + ",,, " + data[0]['long']);
 				processParkingData(data);
 			} else { 
 				alert("No parkings found"); }
@@ -79,6 +88,42 @@ function processParkingData(data) {
 					title: data[i].label
 				});
 	}
+}
+	
+function createSearchControl(parentDiv) {
+	parentDiv.id = "search-box"
+	// Set CSS for the control border
+	var controlUI = document.createElement('div');
+	controlUI.style.backgroundColor = '#fff';
+	controlUI.style.border = '2px solid #fff';
+	controlUI.style.cursor = 'pointer';
+	controlUI.style.marginBottom = '22px';
+	controlUI.style.textAlign = 'center';
+	//controlUI.title = 'Click to recenter the map';
+	parentDiv.appendChild(controlUI);
+
+	// Set CSS for the control interior
+	var controlText = document.createElement('input');
+	controlText.style.color = 'rgb(25,25,25)';
+	controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+	controlText.style.fontSize = '16px';
+	controlText.style.lineHeight = '38px';
+	controlText.style.paddingLeft = '5px';
+	controlText.style.paddingRight = '5px';
+	controlText.innerHTML = 'Center Map';
+	controlUI.appendChild(controlText);
+	
+	attachSearchControl(controlText);
+}
+
+function attachSearchControl (parentDiv) {
+	var AustraliaBounds = new google.maps.LatLngBounds(
+		new google.maps.LatLng(-40, 111),
+		new google.maps.LatLng(-7.5, 157));
+
+	var searchBox = new google.maps.places.SearchBox(parentDiv, {
+		bounds: AustraliaBounds
+	});
 }
 	
 function globalInit() {
