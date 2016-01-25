0e8c2834bfa7ab6281d89525a4b28fcd3a80054f
Stepan Tsymbal <ctepa2000@gmail.com>
2016-01-25 13:58:19 +0700

    Geocoding input added.

--- parker/constants.py
+++ parker/constants.py
@@ -2,3 +2,4 @@ __author__ = 'ctepasiam2'
 
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyA5MZPwIeHVdTYLtZg_o8FVEihohTz7bXw'     # TODO: Configure referrer at Google DevConsole
+GOOGLE_GEOCODING_API_KEY = 'AIzaSyCJeDF2U8wbH4s04NAwq8CElCUb2PGdLgc'      # Not used right now
