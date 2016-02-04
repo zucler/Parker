ebbac5800fb48c3d0b37d37a6b33d9f6655c7ae6
Stepan Tsymbal <ctepa2000@gmail.com>
2016-02-04 22:34:29 +0700

    AWS

--- parker/settings.py
+++ parker/settings.py
@@ -121,7 +121,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-STATIC_PRECOMPILER_OUTPUT_DIR = os.path.join(BASE_DIR, "compiled/")
+STATIC_PRECOMPILER_OUTPUT_DIR = "compiled"
 STATIC_PRECOMPILER_COMPILERS = (
     ('static_precompiler.compilers.SCSS', {
         #"executable": "/home/ctepa/bin/sass",
