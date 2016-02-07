cb3f127796ea07a7b7227702484448aeb8820a0d
Maxim Pak <mpak@switch.tv>
2016-02-07 22:59:12 +1100

    CPRK-27 [Work in progress] Almost finished Early Bird, just need to understand whats wrong with process_dates() function.

--- parker/classes/custom/wilson/rates_sections/early_bird.py
+++ parker/classes/custom/wilson/rates_sections/early_bird.py
@@ -1,45 +1,63 @@
-from parker.classes.custom.wilson.types import RateTypes
+from parker.classes.custom.wilson.rates import WilsonRates
 from parker.classes.core.utils import Utils
 import re
 
 
-class RatesSection():
+class RatesSection(WilsonRates):
     SUPER_EARLY_BIRD_KEY = "super early bird"
     SUPER_EARLY_BIRD_HTML_TITLES = ["super early bird", "super eb", "super earlybird"]
+    EARLY_BIRD_KEY = "early bird"
+    EARLY_BIRD_HTML_TITLES = ["early bird", "eb", "earlybird"]
     ENTRY_EXIT_TIMES_REGEX = "([0-9]{1,2}[pam]{0,2})(?:\:)?([0-9]{1,2}[pam]{0,2})?"
 
     def __init__(self):
+        WilsonRates.__init__(self)
         self.rates_data = ""
-        self.unprocessed_rates_data = ""
         self.processed_rates = dict()
         self.processed_rates[self.SUPER_EARLY_BIRD_KEY] = dict()
+        self.processed_rates[self.EARLY_BIRD_KEY] = dict()
 
     def get_details(self, raw_data):
-        self.rates_data = self.unprocessed_rates_data = raw_data
-        self._process_super_early_bird_rates()
-        #self._process_early_bird_rates()
-        #self.extract_days()  # go throw original rates_data list and pick up every line that has days in it and decide which type it should go to
+        self.rates_data = raw_data
+        self._process_rates(self.SUPER_EARLY_BIRD_HTML_TITLES, self.SUPER_EARLY_BIRD_KEY)
+        self._process_rates(self.EARLY_BIRD_HTML_TITLES, self.EARLY_BIRD_KEY)
+        #self._process_days()
         Utils.pprint(self.processed_rates)
         exit()
 
-    def _process_super_early_bird_rates(self):
+    def _process_days(self):
+        for line in self.rates_data:
+            if self.is_a_day(line):  # Check if a line has days information
+                # Check if days are specific to a certain rate
+                if self._is_title_in_line(line, self.SUPER_EARLY_BIRD_HTML_TITLES):
+                    self.processed_rates[self.SUPER_EARLY_BIRD_KEY]['days'] = self._detect_days_in_range(line)
+                elif self._is_title_in_line(line, self.EARLY_BIRD_HTML_TITLES):
+                    self.processed_rates[self.EARLY_BIRD_KEY]['days'] = self._detect_days_in_range(line)
+                else:
+                    for rate_type in self.processed_rates.keys():
+                        self.processed_rates[rate_type]["days"] = self._detect_days_in_range(line)
+
+    def _process_rates(self, titles_list, dict_key):
         """ Parses raw rates data provided and builds a structured dictionary of information
+
+        Args:
+            titles_list (list): List of either early bird or super early bird titles
+            dict_key (str): Dictionary key to be used to store processed data against
 
         Returns:
             Stores processed data in self.processed_rates dictionary
         """
         i = 0
         for line in self.rates_data:
+            next_line = ""
             if not i + 1 == len(self.rates_data):
                 next_line = self.rates_data[i + 1]
 
             # If current line is a header, and next one is price
-            if self._is_title_in_line(line, self.SUPER_EARLY_BIRD_HTML_TITLES) and Utils.string_found("$", next_line):
-                self.processed_rates[self.SUPER_EARLY_BIRD_KEY]["price"] = next_line
-                self.unprocessed_rates_data.pop(self.unprocessed_rates_data.index(line))
-                self.unprocessed_rates_data.pop(self.unprocessed_rates_data.index(next_line))
+            if self._do_save_rates(titles_list, dict_key, line, next_line):
+                self.processed_rates[dict_key]["price"] = next_line
             # Else if current line is Entry & Exit times
-            elif self._is_title_in_line(line, self.SUPER_EARLY_BIRD_HTML_TITLES) and Utils.string_found("entry", line.lower()):
+            elif self._do_save_times_data(titles_list, dict_key, line):
                 times_list = line.split(",")
 
                 entry_times = []
@@ -50,16 +68,48 @@ class RatesSection():
                 for exit_list in re.compile(self.ENTRY_EXIT_TIMES_REGEX).findall(times_list[1]):
                     exit_times.append(list(filter(None, exit_list)))  # Filtering out empty strings
 
-                self.processed_rates[self.SUPER_EARLY_BIRD_KEY]["entry start"] = Utils.convert_to_24h_format(":".join(entry_times[0]))
-                self.processed_rates[self.SUPER_EARLY_BIRD_KEY]["entry end"] = Utils.convert_to_24h_format(":".join(entry_times[1]))
-                self.processed_rates[self.SUPER_EARLY_BIRD_KEY]["exit start"] = Utils.convert_to_24h_format(":".join(exit_times[0]))
-                self.processed_rates[self.SUPER_EARLY_BIRD_KEY]["exit end"] = Utils.convert_to_24h_format(":".join(exit_times[1]))
-
-                self.unprocessed_rates_data.pop(self.unprocessed_rates_data.index(line))
+                self.processed_rates[dict_key]["entry start"] = Utils.convert_to_24h_format(":".join(entry_times[0]))
+                self.processed_rates[dict_key]["entry end"] = Utils.convert_to_24h_format(":".join(entry_times[1]))
+                self.processed_rates[dict_key]["exit start"] = Utils.convert_to_24h_format(":".join(exit_times[0]))
+                self.processed_rates[dict_key]["exit end"] = Utils.convert_to_24h_format(":".join(exit_times[1]))
 
             i += 1
 
+    def _do_save_rates(self, titles_list, dict_key, line, next_line):
+        if Utils.string_found("$", next_line):
+            if self._is_title_in_line(line, titles_list):
+                return True
+            elif self._is_single_rate_parking(dict_key):
+                return True
+
+        return False
+
+    def _do_save_times_data(self, titles_list, dict_key, line):
+        if Utils.string_found("entry", line.lower()):
+            if self._is_title_in_line(line, titles_list):
+                return True
+            elif self._is_single_rate_parking(dict_key):
+                return True
+
+        return False
+
+    def _is_single_rate_parking(self, dict_key):
+        # Super EB does not exist, everything default to just EB
+        if dict_key == self.EARLY_BIRD_KEY and "price" not in self.processed_rates[self.SUPER_EARLY_BIRD_KEY]:
+            return True
+
+        return False
+
     def _is_title_in_line(self, line, header_titles):
+        """ Checks if HTML line provied has a section title in it
+
+        Args:
+            line (str): subject to search line
+            header_titles (list): list of title variations
+
+        Returns:
+
+        """
         for title in header_titles:
             if Utils.string_found(title, line.lower()):
                 return True
