c344ea262312fd9c0afc9b551220a08142d581fb
Maxim Pak <mpak@switch.tv>
2016-02-09 20:26:26 +1100

    CPRK-27 Casual rates parser

--- parker/classes/custom/wilson/rates_sections/casual.py
+++ parker/classes/custom/wilson/rates_sections/casual.py
@@ -9,38 +9,34 @@ class RatesSection(WilsonRates):
         self.processed_rates = dict()
 
     def get_details(self, section_data):
-        Utils.pprint(section_data)
-        exit()
         self.processed_rates['entry_start'] = "00:00"
         self.processed_rates['exit_end'] = "23:59"
-        self.processed_rates['days_range'] = ""
+        self.processed_rates['days'] = ""
+        self.processed_rates['prices'] = dict()
 
-        # Taken from rates.py REFACTOR
-        if Utils.string_found('hrs', line):
-            hours_str = self._format_hours_line(line)
-            prices_str = self._format_prices_line(next_line)
+        i = 0
+        current_hourly_minutes = 0
+        for line in section_data:
+            if Utils.string_found('hrs', line):
+                if not i + 1 == len(section_data):
+                    next_line = section_data[i + 1]
 
-            hours_arr = hours_str.split(" - ")
-            if len(hours_arr) == 2:
-                offset = float(hours_arr[1]) - float(hours_arr[0])
-                current_hourly_minutes += 30
-                prices[current_hourly_minutes] = prices_str
+                hours_str = self._format_hours_line(line)
+                prices_str = self._format_prices_line(next_line)
 
-                if offset == 1.0:
+                hours_arr = hours_str.split(" - ")
+                if len(hours_arr) == 2:
+                    offset = float(hours_arr[1]) - float(hours_arr[0])
                     current_hourly_minutes += 30
-                    prices[current_hourly_minutes] = prices_str
-            else:
-                prices[1440] = prices_str  # 1440 is 24 hours in minutes
-        """
-        {   'early bird': {   'days': [1, 2, 3, 4, 5],
-                      'entry end': '09:30',
-                      'entry start': '06:00',
-                      'exit end': '19:00',
-                      'exit start': '15:00',
-                      'price': '$15.00'},
-        """
+                    self.processed_rates['prices'][current_hourly_minutes] = prices_str
 
-        rate_details['days_range'] = []  # casual rate is applicable everyday
+                    if offset == 1.0:
+                        current_hourly_minutes += 30
+                        self.processed_rates['prices'][current_hourly_minutes] = prices_str
+                else:
+                    self.processed_rates['prices'][1440] = prices_str  # 1440 is 24 hours in minutes
+
+            i += 1
 
         return rate_details
 
