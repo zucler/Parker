8fa0002f1d8ffbe521352855b5bc427a4b79dcf1
Maxim Pak <zucler@gmail.com>
2016-02-13 17:09:28 +1100

    CPRK-15 More work in progress

--- parker/classes/custom/wilson/rates_sections/weekend.py
+++ parker/classes/custom/wilson/rates_sections/weekend.py
@@ -1,9 +1,23 @@
-from parker.classes.custom.wilson.types import RateTypes
+from parker.classes.custom.wilson.rates import WilsonRates
+from parker.classes.custom.wilson.rates_sections import casual
+from parker.classes.core.utils import Utils
 
+class RatesSection(WilsonRates):
+    LABEL = "Weekend"
 
-class RateType(RateTypes):
     def __init__(self):
-        """
-        Returns:
+        WilsonRates.__init__(self)
+        self.rates_data = ""
+        self.processed_rates = dict()
 
-        """
+    def get_details(self, section_data, parking_rates):
+        Utils.pprint(section_data)
+
+        # Ignore if casual rates apply
+        if len(section_data) == 1:
+            if Utils.string_found("casual rates apply", section_data[0].lower()):
+                return
+
+
+
+
