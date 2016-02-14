0c8aace6514e5ee4e0fe14186e5d6ba9b12879a1
Maxim Pak <zucler@gmail.com>
2016-02-14 17:21:49 +1100

    CPRK-15 Best scrappy code carparker seen so far

--- parker/classes/custom/wilson/rates_sections/casual.py
+++ parker/classes/custom/wilson/rates_sections/casual.py
@@ -15,36 +15,10 @@ class RatesSection(WilsonRates):
         self.processed_rates['exit_end'] = "23:59"
         self.processed_rates['days'] = ""
         self.processed_rates['prices'] = dict()
-        self.processed_rates['label'] = self.LABEL
 
-        i = 0
-        processed_lines = []
-        current_hourly_minutes = 0
-        for line in section_data:
-            if Utils.string_found('hrs', line):
-                if not i + 1 == len(section_data):
-                    next_line = section_data[i + 1]
+        self._extract_hourly_rates(section_data)
 
-                hours_str = self._format_hours_line(line)
-                prices_str = self._format_prices_line(next_line)
-
-                hours_arr = hours_str.split(" - ")
-                if len(hours_arr) == 2:
-                    offset = float(hours_arr[1]) - float(hours_arr[0])
-                    current_hourly_minutes += 30
-                    self.processed_rates['prices'][current_hourly_minutes] = prices_str
-
-                    if offset == 1.0:
-                        current_hourly_minutes += 30
-                        self.processed_rates['prices'][current_hourly_minutes] = prices_str
-                else:
-                    self.processed_rates['prices'][1440] = prices_str  # 1440 is 24 hours in minutes
-
-                processed_lines.append(line)
-                processed_lines.append(next_line)
-            i += 1
-
-        for line_to_remove in processed_lines:
+        for line_to_remove in self.processed_lines:
             section_data.remove(line_to_remove)
 
         parking_rates[self.LABEL] = self.processed_rates
