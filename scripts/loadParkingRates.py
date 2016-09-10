0828ba131195c85c742c4f20c0e0b21d29e8ce60
Maxim Pak <zucler@gmail.com>
2016-09-10 21:10:10 +1000

    CPRK-35 Reading cached HTML in loadParkingRates.py script

--- scripts/loadParkingRates.py
+++ scripts/loadParkingRates.py
@@ -8,24 +8,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 import parker.wsgi
 from parker.classes.core.utils import Utils
 from parker.models import Parking, RateType, RatePrice
+from django.conf import settings
 
 sys.path.append('/srv/prod/carparker')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parker.settings")
 
-# carparkings = Parking.objects.all()
-carparkings = Parking.objects.filter(parkingID=7)
-for carpark in carparkings:
-    mod = __import__("parker.classes.custom." + carpark.parking_type.lower() + ".rates_retriever", fromlist=['RatesRetriever'])
-    RatesRetriever = getattr(mod, 'RatesRetriever')
-    url = carpark.uri
-    # use firefox to get page with javascript generated content
-    with closing(Firefox()) as browser:
-        browser.get(url)
-        # wait for the page to load
-        WebDriverWait(browser, timeout=10).until(
-                lambda x: x.find_element_by_class_name('rates'))
+import os
+for html_file in os.listdir(settings.HTML_CACHE_DIRECTORY):
+    if html_file.endswith(".html"):
+        rates_file = open(os.path.join(settings.HTML_CACHE_DIRECTORY, html_file), "r")
+        rates_html = rates_file.read()
+        rates_file.close()
+
+        carparkID = html_file[settings.HTML_FILE_PREFIX_LENGTH:-settings.HTML_FILE_SUFFIX_LENGTH]
+        carpark = Parking.objects.filter(parkingID=carparkID)
+        mod = __import__("parker.classes.custom." + carpark.parking_type.lower() + ".rates_retriever",
+                         fromlist=['RatesRetriever'])
+        RatesRetriever = getattr(mod, 'RatesRetriever')
         # store it as string variable
         parser = RatesRetriever()
-        parser.update_rates(carpark, browser.page_source)
-        exit()
+        parser.update_rates(carpark, rates_html)
 
