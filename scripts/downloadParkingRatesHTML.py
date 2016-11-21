b8c7adaec9121592e95b1dd4789b90cdd204fccc
Maxim Pak <mpak@switch.tv>
2016-11-21 14:52:24 +1100

    Using phantomjs to download HTML

--- scripts/downloadParkingRatesHTML.py
+++ scripts/downloadParkingRatesHTML.py
@@ -2,8 +2,7 @@ import os
 import sys
 from contextlib import closing
 
-from selenium.webdriver import Firefox
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import PhantomJS
 
 sys.path.append('/carparker')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parker.settings")
@@ -12,20 +11,13 @@ import parker.wsgi
 from parker.models import Parking, RateType, RatePrice
 from django.conf import settings
 
-
 carparkings = Parking.objects.all()
-#carparkings = Parking.objects.filter(parkingID=7)
+# carparkings = Parking.objects.filter(parkingID=7) # use for tests
 for carpark in carparkings:
-    mod = __import__("parker.classes.custom." + carpark.parking_type.lower() + ".rates_retriever", fromlist=['RatesRetriever'])
-    RatesRetriever = getattr(mod, 'RatesRetriever')
     url = carpark.uri
-    # use firefox to get page with javascript generated content
-    with closing(Firefox()) as browser:
+    with closing(PhantomJS()) as browser:
         browser.get(url)
-        # wait for the page to load
-        WebDriverWait(browser, timeout=10).until(
-                lambda x: x.find_element_by_class_name('rates'))
 
         # store it as string variable
-        with open(settings.HTML_CACHE_DIRECTORY + "/carparkID_" + str(carpark.parkingID) + ".html", "w") as out_file:
-            out_file.write(browser.page_source)
+        with open(settings.HTML_CACHE_DIRECTORY + "/carparkID_" + str(carpark.parkingID) + ".html", "wb") as out_file:
+            out_file.write(browser.page_source.encode('utf-8'))
